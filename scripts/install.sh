ab9ef5f4ef3dfa47fc6332ba4f82c83b8c8ead69
Sree <62918878+NunnaRupaSri@users.noreply.github.com>
2025-09-17 14:37:24 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,19 @@
-#!/bin/bash 
-# This installs the CodeDeploy agent and its prerequisites on Ubuntu 22.04.  
-sudo apt-get update 
-sudo apt-get install ruby-full ruby-webrick wget -y 
-cd /tmp 
-wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb 
-mkdir codedeploy-agent_1.3.2-1902_ubuntu22 
-dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22 
-sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control 
-dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/ 
-sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb 
-systemctl list-units --type=service | grep codedeploy 
-sudo service codedeploy-agent status
+#!/bin/bash
+apt update -y
+apt install -y nginx ruby-full wget awscli
+
+# Install CodeDeploy agent
+cd /tmp
+wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+
+# Create application directory
+mkdir -p /var/www/html
+chown www-data:www-data /var/www/html
+
+# Start services
+systemctl start nginx
+systemctl enable nginx
+service codedeploy-agent start
+service codedeploy-agent status
