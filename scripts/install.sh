3fcecc2c5afa4ba59c1c2d3f9417e5decd89d366
Sree <62918878+NunnaRupaSri@users.noreply.github.com>
2025-09-17 14:55:10 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-apt update -y
-apt install -y nginx ruby-full wget awscli
-
-# Install CodeDeploy agent
-cd /tmp
-wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
-
-# Create application directory
-mkdir -p /var/www/html
-chown www-data:www-data /var/www/html
-
-# Start services
-systemctl start nginx
-systemctl enable nginx
-service codedeploy-agent start
-service codedeploy-agent status
-#!/bin/bash
 sudo apt update -y
 sudo apt install -y nginx ruby-full wget awscli
 
@@ -37,4 +18,4 @@ sudo systemctl enable nginx
 
 # Start CodeDeploy agent
 sudo service codedeploy-agent start
-update-rc.d codedeploy-agent defaults
+sudo update-rc.d codedeploy-agent defaults
