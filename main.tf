59254e95c39c9db212ee4ad42bd6e243d09e603f
Sree <62918878+NunnaRupaSri@users.noreply.github.com>
2025-09-17 17:47:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -399,6 +399,26 @@ resource "aws_codebuild_project" "app_build_project" {
   }
 }
 
+resource "aws_iam_role" "codedeploy_service_role" {
+  name = "codedeploy-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_service_role_policy" {
+  role       = aws_iam_role.codedeploy_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
 resource "aws_codedeploy_app" "app" {
   name             = "html-app"
   compute_platform = "Server"
@@ -407,7 +427,7 @@ resource "aws_codedeploy_app" "app" {
 resource "aws_codedeploy_deployment_group" "app_deployment_group" {
   app_name              = aws_codedeploy_app.app.name
   deployment_group_name = "html-app-deployment-group"
-  service_role_arn      = aws_iam_role.codepipeline_role.arn
+  service_role_arn      = aws_iam_role.codedeploy_service_role.arn
 
   ec2_tag_set {
     ec2_tag_filter {
