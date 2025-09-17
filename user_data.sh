5d79cd363fbba957efd8a0389e6c80e2d0b7fbe4
Sree <62918878+NunnaRupaSri@users.noreply.github.com>
2025-09-17 13:21:45 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-yum update -y
-yum install -y nodejs npm ruby wget awscli
+apt update -y
+apt install -y nodejs npm ruby wget awscli
 
 # Install CodeDeploy agent
-cd /home/ec2-user
+cd /home/ubuntu
 wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
 
 # Create application directory
 mkdir -p /var/www/html
-chown ec2-user:ec2-user /var/www/html
+chown ubuntu:ubuntu /var/www/html
 
 # Start and enable CodeDeploy agent
-service codedeploy-agent start
-chkconfig codedeploy-agent on
+systemctl start codedeploy-agent
+systemctl enable codedeploy-agent
 
 # Wait and restart agent
 sleep 10
-service codedeploy-agent restart
+systemctl restart codedeploy-agent
 
 # Tag instance for CodeDeploy
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
