6b03471d340b02067a28bf71cb43e581e23a8949
Sree <62918878+NunnaRupaSri@users.noreply.github.com>
2025-09-17 13:39:19 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ -f /var/run/nodejs-app.pid ]; then
-  kill $(cat /var/run/nodejs-app.pid) || true
-  rm -f /var/run/nodejs-app.pid
-fi
-pkill -f "npm start" || true
-pkill -f "node" || true
+npm stop || true
